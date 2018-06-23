9ac40b31c5f4a4fd2c60522c85aeee14ce6f51b8
PenguinAgen <albertsgarde@gmail.com>
2018-06-23 22:03:59 +0200

    Add gain and half tone offset to OscillatorModule

--- Synth/Synth/ModuleBoard/Modules/OscillatorModule.cs
+++ Synth/Synth/ModuleBoard/Modules/OscillatorModule.cs
@@ -11,22 +11,29 @@ namespace SynthLib.Board.Modules
     {
         private IOscillator oscillator;
 
+        private float gain;
+
+        private float frequencyMultiplier;
+
         public override Connections Inputs { get; }
 
         public override Connections Outputs { get; }
 
         public override string Type { get; } = "Oscillator";
 
-        public OscillatorModule(IOscillator oscillator, int outputs)
+        public OscillatorModule(IOscillator oscillator, int outputs, float gain = 1, float halfToneOffset = 0)
         {
-            this.oscillator = oscillator;
+            this.oscillator = oscillator.Clone();
+            frequencyMultiplier = (float) Math.Pow(2, (1 / 12d) * halfToneOffset);
+            this.gain = gain;
             Inputs = new ConnectionsArray(0);
             Outputs = new ConnectionsArray(outputs);
         }
 
         public override float[] Process(float[] inputs)
         {
-            var next = oscillator.NextValue(440);
+            var frequency = 440d;
+            var next = oscillator.NextValue(frequency * frequencyMultiplier);
             var output = new float[Outputs.Count];
             for (int i = 0; i < output.Length; ++i)
                 output[i] = next;
