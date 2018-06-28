649db1d7b3ea97ebdcfe4022aed8bab283ba0ce1
PenguinAgen <albertsgarde@gmail.com>
2018-06-28 18:57:02 +0200

    Add Flip module

---
+++ Synth/Synth/Board/Modules/Flip.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynthLib.Board.Modules
+{
+    /// <summary>
+    /// Flips the incoming signal. *-1
+    /// </summary>
+    public class Flip : Module
+    {
+        public override Connections Inputs { get; }
+
+        public override Connections Outputs { get; }
+
+        public override string Type { get; } = "Flip";
+
+        public Flip()
+        {
+            Inputs = new ConnectionsArray(1);
+            Outputs = new ConnectionsArray(1);
+        }
+
+        public override void Next()
+        {
+        }
+
+        public override float[] Process(float[] inputs)
+        {
+            return new float[] { inputs[0] * -1 };
+        }
+    }
+}
