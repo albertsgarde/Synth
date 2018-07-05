659c58541957f414e5c16fd7567adec077794f64
PenguinAgen <albertsgarde@gmail.com>
2018-07-05 12:44:30 +0200

    Replace Flip module with a Flip effect

---
+++ Synth/Synth/Effects/Flip.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynthLib.Effects
+{
+    public class Flip : IEffect
+    {
+        public IEffect Clone()
+        {
+            return new Flip();
+        }
+
+        public float Next(float input)
+        {
+            return input * -1;
+        }
+    }
+}
