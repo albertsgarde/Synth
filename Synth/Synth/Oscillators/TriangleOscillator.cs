f64912b3d1ef3f86d33b78d0b294f15ee9b93f5e
PenguinAgen <albertsgarde@gmail.com>
2018-07-08 21:51:33 +0200

    Add TriangleOscillator

---
+++ Synth/Synth/Oscillators/TriangleOscillator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynthLib.Oscillators
+{
+    public class TriangleOscillator : IOscillator
+    {
+        public int SampleRate { get; }
+
+        private float frequency;
+
+        private float valueIncrement;
+
+        private float curValue;
+
+        public TriangleOscillator(float frequency, float startValue = 0, int sampleRate = 44100)
+        {
+            SampleRate = sampleRate;
+            Frequency = frequency;
+            curValue = startValue;
+        }
+
+        public float Frequency
+        {
+            get => frequency;
+            set
+            {
+                frequency = value;
+                valueIncrement = frequency / SampleRate;
+            }
+        }
+
+        public void Next()
+        {
+            curValue += valueIncrement;
+            curValue %= 1;
+        }
+
+        public float CurrentValue(float min = -1F, float max = 1)
+        {
+            float result;
+            if (curValue <= 0.5f)
+                result = curValue;
+            else
+                result = 0.5f - curValue;
+            return result * (max - min) + min;
+        }
+
+        public float NextValue(float min, float max = 1)
+        {
+            Next();
+            return CurrentValue(min, max);
+        }
+
+        public float NextValue()
+        {
+            Next();
+            return curValue <= 0.5f ? curValue : 0.5f - curValue;
+        }
+
+        public IOscillator Clone()
+        {
+            return new TriangleOscillator(Frequency, curValue, SampleRate);
+        }
+
+        public void Reset()
+        {
+            curValue = 0;
+        }
+    }
+}
