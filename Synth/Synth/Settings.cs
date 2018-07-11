434a945db1705bf04069e3a0f79152cabe53abef
PenguinAgen <albertsgarde@gmail.com>
2018-07-11 18:20:07 +0200

    Add a very basic static Settings class and a settings directory

---
+++ Synth/Synth/Settings.cs
@@ -0,0 +1,21 @@
+﻿using Stuff;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynthLib
+{
+    public static class Settings
+    {
+        private readonly static SettingsManager settings;
+
+        static Settings()
+        {
+            settings = SettingsLoader.LoadSettings();
+        }
+
+        public static string GetString(string category, string key, int index = 0) => settings.GetString(category, key, index);
+    }
+}
