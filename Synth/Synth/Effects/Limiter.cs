3a2f3ab26cf41c571f757e80120375c73d086a54
PenguinAgen <albertsgarde@gmail.com>
2018-08-18 18:04:12 +0200

    Add Limiter effect, which functions as basic distortion

---
+++ Synth/Synth/Effects/Limiter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using SynthLib.Data;
+using Stuff;
+
+namespace SynthLib.Effects
+{
+    public class Limiter : Effect
+    {
+        public override string Type => "Limiter";
+
+        public override int Values => 0;
+
+        public float Limit { get; }
+
+        public Limiter(float limit)
+        {
+            Limit = limit;
+        }
+
+        public override Effect Clone()
+        {
+            return new Limiter(Limit);
+        }
+
+        public override Effect CreateInstance(XElement element, SynthData data)
+        {
+            var limit = InvalidEffectSaveElementException.ParseFloat(element.Element("limit"));
+            return new Boost(limit);
+        }
+
+        protected override float Next(float[] input)
+        {
+            if (input[0] > Limit)
+                return Limit;
+            else if (input[0] < -Limit)
+                return -Limit;
+            else
+                return input[0];
+        }
+
+        public override XElement ToXElement(string name)
+        {
+            var element = base.ToXElement(name);
+            element.AddValue("limit", Limit);
+            return element;
+        }
+    }
+}
